4f98a956d70fce45bb49372e81797be8db1dda7b
tappiola <karamorina@gmail.com>
2022-07-11 22:58:49 +0100

    refactoring

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,20 +1,15 @@
-import {Carousel} from "./Carousel";
 import {Pills} from "./Pills";
 
-export const ProjectCard = ({project, project: {screenshots, title, description, pills}}) => {
+export const ProjectCard = ({project, project: {screenshot, title, description, pills}}) => {
     console.log({project});
 
     return <div className="p-10 m-10 bg-stone-700 rounded-2xl flex">
-        {/*<h1 className=" text-stone-300 text-4xl my-3 ">Real estate agency website</h1>*/}
-        {/*<div>*/}
-        {/*<Carousel images={screenshots}/>*/}
             <img
-                src={screenshots[0]}
+                src={screenshot}
                 className="block w-2/5"
                 alt="Wild Landscape"
                 style={{aspectRatio: "16/9"}}
             />
-        {/*</div>*/}
         <div className="pl-20">
             <h1 className="text-stone-300 text-4xl my-3">{title}</h1>
             {description.map(p => <p className="text-stone-300">{p}</p>)}
--- src/Projects.js
+++ src/Projects.js
@@ -1,20 +1,12 @@
-
-import ondat1 from "./screenshots/ondat1.png";
-import ondat2 from "./screenshots/ondat2.png";
-import ondat3 from "./screenshots/ondat3.png";
-import ondat4 from "./screenshots/ondat4.png";
-import ondat5 from "./screenshots/ondat5.png";
-import ondat01 from "./screenshots/ondat01.png";
-import shop0 from "./screenshots/shop0.png";
-import wordle5 from "./screenshots/wordle5.png";
-import realEstate00 from './screenshots/realEstate00.png';
-import realEstate0 from './screenshots/realEstate0.png';
-import realEstate1 from './screenshots/real-estate1.png';
-import realEstate2 from './screenshots/real-estate2.png';
-import realEstate3 from './screenshots/realEstate3.png';
-import stocks0 from './screenshots/stocks0.png';
 import {ProjectCard} from "./ProjectCard";
-import codenames1 from './screenshots/codenames1.png';
+import ondat from "./screenshots/ondat.png";
+import shop from "./screenshots/shop.png";
+import wordle from "./screenshots/wordle.png";
+import realEstate from './screenshots/real_estate.png';
+import stocks from './screenshots/stocks.png';
+import tasks from './screenshots/tasks.png';
+import scandipwa from './screenshots/scandipwa.png';
+import codenames from './screenshots/codenames.png';
 
 const PROJECTS = [
     {
@@ -22,37 +14,49 @@ const PROJECTS = [
         description: ['Web app allowing to search for real estate properties, filter them and view on interactive map.',
             'Selected features: user authentication, animations, responsive design, dynamic list loading, react lazy loading, custom carousel, notification stack, images gallery, skeleton loaders.'],
         pills: ['React', 'React Router v6', 'redux-toolkit', 'SASS', 'TypeScript', 'react-transition-library', 'Express', 'GraphQL', 'sequelize', 'JWT', 'Docker', 'docker-compose'],
-        screenshots: [realEstate00]
+        screenshot: realEstate
     },
     {
         title: 'Portal for Kubernetes clusters monitoring',
         description: ['Portal for monitoring and configuring Kubernetes clusters.'],
         pills: ['React', 'React hooks', 'React Router v6', 'react-query', 'react-hook-form', 'styled-components', 'carbon-components', 'Typescript', 'GraphQL', 'Jest', 'React Testing Library', 'Frontegg', 'plop', 'Storybook', 'Growthbook', 'Github Actions', 'dependabot', 'Nest.js', 'Prisma', 'Kubernetes', 'Docker', 'Pub / Sub', 'Redis', 'PostgreSQL', 'JWT'],
-        screenshots: [ondat01, ondat1, ondat2, ondat3, ondat4, ondat5]
+        screenshot: ondat
+    },
+    {
+        title: 'E-commerce website',
+        description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
+        pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
+        screenshot: scandipwa
+    },
+    {
+        title: 'Tasks management app',
+        description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
+        pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
+        screenshot: tasks
     },
     {
         title: 'Online game',
         description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
         pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
-        screenshots: [codenames1]
+        screenshot: codenames
     },
     {
         title: 'Online shop',
         description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
         pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
-        screenshots: [shop0]
+        screenshot: shop
     },
     {
         title: 'Wordle',
         description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
         pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
-        screenshots: [wordle5]
+        screenshot: wordle
     },
     {
         title: 'Stocks',
         description: ['Multiplayer game for guessing words by making associations to a keyword.', 'Features: pseudo-random game fields generation, complex game progress and winner detection logic, webSockets, multiple languages, mobile devices support.'],
         pills: ['React', 'React Router', 'React Hooks', 'React Context', 'Firestore', 'clsx'],
-        screenshots: [stocks0]
+        screenshot: stocks
     },
 ];
 
