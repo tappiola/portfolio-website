201dac870a6b865eeba8103576457e43f4b574e6
tappiola <karamorina@gmail.com>
2022-07-12 16:32:46 +0100

    add about me description text

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Introduction} from "./Introduction";
 import {Testimonials} from "./Testimonials";
 import {Projects} from "./Projects";
 import {Career} from "./Career";
+import {AboutMe} from "./AboutMe";
 
 function App() {
   return (
       <div className="py-12 bg-stone-900">
           <Header/>
           <Introduction/>
+          <AboutMe/>
           <p className="text-3xl text-indigo-100 font-mono my-12 mx-auto">"Give me the designs, I'll get the rest done"</p>
           <Projects/>
           <Career/>
