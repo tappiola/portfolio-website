c43461f3edb6111192537965ba74a6c2ae0520bf
tappiola <karamorina@gmail.com>
2022-07-13 11:14:05 +0100

    add about me description text

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,4 +1,10 @@
 import {AboutMeSection} from "./AboutMeSection";
+import backend from './screenshots/back-end-developer.png';
+import soft from './screenshots/soft.png';
+import react from './screenshots/react.jpeg';
+import quality from './screenshots/quality.jpeg';
+import css from './screenshots/css.png';
+
 const ABOUT_ME = [
     {
         title: 'How I work',
@@ -8,6 +14,7 @@ const ABOUT_ME = [
             'Able to work in rapidly changing startup environments efficiently',
             'Comfortable working in small teams, not afraid to take responsibility and perform a broad scope of tasks',
             'Constantly suggest the ways to improve the project (from code quality to UX and processes)'],
+        image: soft,
     },
     {
         title: 'Making it visually perfect',
@@ -19,50 +26,53 @@ const ABOUT_ME = [
             'Have experience of creating complex animated UI components from scratch (e.g. carousel, swipe to delete, fullscreen image zoom, image gallery, notifications stack, tooltips)',
             'I follow accessibility guidelines',
         ],
+        image: css,
     },
     {
-        title: 'I work with React and other modern JS tooling',
+        title: 'React and other modern JS tooling',
         features: [
-            'Have In-depth knowledge of both React class components and React Hooks',
-            'I Know how to use React hooks and lifecycle methods to provide the best performance and minimise DOM rerenders',
-            'I use lazy loading and code splitting to optimise applications performance',
-            'I generate Lighthouse reports for my projects to make sure they follow the recommended guidelines.',
-            'I bring centralised storage such as Redux or React Context to projects to avoid props drilling',
-            'I efficiently connect FEs to backend using both Restful and GraphQL APIs, add proper error handling and loaders to UI and use React hooks and lifecycle methods to avoid unnecessary API calls.',
-            'Comfortable working with BOM, DOM events and Vanilla JS.',
-            'I can create FE application using Svelte as well.',
-            'I allocate time to investigating new tooling and try to choose wisely between writing features on your own and bringing new packages to project.',
+            'I use React and Svelte for building user interfaces',
+            'Excellent knowledge of React hooks and lifecycle methods',
+            'I care about applications performance',
+            'I use centralised storage in applications when needed',
+            'I connect FEs to both Restful and GraphQL APIs',
+            'Good knowledge of BOM, DOM events and Vanilla JS',
+            'I pay attention to edge cases and add proper error handling'
         ],
+        image: react,
     },
     {
         title: 'I care about code quality',
         features: [
-            'I can’t imagine development in a team without PRs and code reviews',
-            'I use tools such as Eslint and StyleLint to bring code formatting to a single standard',
-            'I use prop-types or Typescript with React to avoid typing errors.',
+            'PRs and code reviews as a standard',
             'I use tools such as plop to standardise components naming and structure',
-            'I try to use latest versions of libraries and upgrade versions when possible with help of tools such as dependabot',
-            'I write unit tests for my code and check that tests coverage is sufficient',
-            'I use mocks and Storybook for development/testing to ensure correct behaviour for edge cases',
-            'I encourage usage of GitHub actions or similar tooling to execute basic checks on pull requests and ensure no broken code gets merged',
+            'Tools such as Eslint and StyleLint for consistent code formatting',
+            'Prop-types and Typescript to avoid typing errors.',
+            'I aim to use latest versions of libraries',
+            'I write unit tests and check tests coverage',
+            'Mocks and Storybook help me test edge cases',
+            'I use GitHub actions for pull requests checks',
             'I execute basic e2e tests before making a PR',
         ],
+        image: quality,
     },
     {
         title: 'More than just a frontend developer',
         features: [
-            '10+ years in IT give me very broad technical background.',
-            'My previous career in QA influences my approach to development and results in better quality of the features and better code coverage. QA engineers who work with me admit that they find much less bugs when testing my tasks.',
-            'As a former QA engineer on a big data project, I have good knowledge of both relational and non-relational databases.',
-            'I have broad knowledge of automation testing tooling (Cypress, pytest, Selenium, JMeter) and best approaches to testing.',
-            'I have enough skills to write not too complicated backends (i.e. create restful API / GraphQL schema and support for CRUD operations using database for persistent storage). I’ve been using ORMs such as Sequielize and Prisma and created backends using python (Django Rest Framework) and JavaScript (Express Server, Node.js, Nest.js).',
-            'I work with Docker, docker-compose and know basics of Kubernetes.',
+            'Very broad technical background',
+            'QA experience helps me develop features of better quality',
+            'Good knowledge of databases and big data project experience',
+            'Broad knowledge of automation testing tooling',
+            'I have enough skills to develop a backend with REST / GraphQL APIs and DB layer',
+            'Docker, docker-compose and basic Kubernetes knowledge',
+            'Apps deployment with AWS, Firebase, etc.'
         ],
+        image: backend,
     },
 ];
 
 export const AboutMe = () => {
-    return <div className="text-stone-200 px-10">
-        {ABOUT_ME.map(({title, features}) => <AboutMeSection key={title} title={title} features={features}/> )}
+    return <div className="text-stone-200 px-10 flex flex-wrap justify-center">
+        {ABOUT_ME.map(({title, features, image}) => <AboutMeSection key={title} title={title} features={features} img={image}/> )}
     </div>
 }
--- src/AboutMeSection.js
+++ src/AboutMeSection.js
@@ -1,10 +1,13 @@
 import { ReactComponent as CheckMark} from './CheckMark.svg';
 
-export const AboutMeSection = ({title, features}) => {
-    return <div className="bg-stone-700 rounded-2xl p-10 w-1/2 my-5">
-            <h2 className="text-indigo-400 mb-5 text-2xl">{title}</h2>
+export const AboutMeSection = ({title, features, img}) => {
+    return <div className="basis-1/3">
+        <div className="bg-stone-700 rounded-2xl p-10 my-5 mx-5">
+        <img src={img}/>
+            <h2 className="text-indigo-400 my-5 text-2xl">{title}</h2>
             <ul style={{fontFamily: "Ubuntu-Regular"}}>
                 {features.map((feature, i) => <li key={i} className="flex my-5 items-start"><CheckMark/>{feature}</li>)}
             </ul>
         </div>
+    </div>
     }
