e812f49d99ce8e4c4b48b4fd075b9bc920948a0e
tappiola <karamorina@gmail.com>
2022-07-13 12:33:19 +0100

    add about me description text

--- src/AboutMeSection.js
+++ src/AboutMeSection.js
@@ -5,7 +5,7 @@ export const AboutMeSection = ({title, features, img}) => {
         <div className="bg-stone-700 rounded-2xl p-10 my-5 mx-5">
         <img src={img}/>
             <h2 className="text-indigo-400 my-5 text-2xl">{title}</h2>
-            <ul style={{fontFamily: "Ubuntu-Regular"}}>
+            <ul style={{fontFamily: "Ubuntu-Regular"}} className="text-stone-400">
                 {features.map((feature, i) => <li key={i} className="flex my-5 items-start"><CheckMark/>{feature}</li>)}
             </ul>
         </div>
