d030c9a6edeabe2f86d15ea2a905ac297e45d19d
tappiola <tatiana@util.co>
2025-08-31 11:35:23 +0100

    Integrate DataDog

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { datadogRum } from '@datadog/browser-rum';
+import { reactPlugin } from '@datadog/browser-rum-react';
 import { createFileRoute } from '@tanstack/react-router';
 import { useEffect, useRef, useState } from 'react';
 import Header, { MENU } from '../Header.tsx';
@@ -60,6 +62,27 @@ function RouteComponent() {
 
     sections.forEach(({ el }) => observer.observe(el));
     return () => observer.disconnect();
+  }, []);
+
+  useEffect(() => {
+    // Don’t run on server
+    if (typeof window === 'undefined') return;
+
+    // Skip in dev
+    if (import.meta.env.DEV) return;
+
+    datadogRum.init({
+      applicationId: '3ed1569f-b8ba-4a37-9ebb-8f93213c3d1f',
+      clientToken: 'pubc53b8fb15b6c7f880f3f24a7b34e918c',
+      site: 'datadoghq.com',
+      service: 'frontend',
+      env: 'prod',
+      version: '1.0.0',
+      sessionSampleRate: 100,
+      sessionReplaySampleRate: 100,
+      defaultPrivacyLevel: 'mask-user-input',
+      plugins: [reactPlugin({ router: false })],
+    });
   }, []);
 
   return (
